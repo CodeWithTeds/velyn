3f57529a4507cd4311215c524f7b50625d056a0e
RLASH18 <lacdangryan18@gmail.com>
2026-05-17 18:54:32 +0800

    refactor(food): use explicit exports for food4 template

--- src/components/templates/food/index.ts
+++ src/components/templates/food/index.ts
@@ -1,5 +1,5 @@
 export { Food1Poster, Food1EditorPage, food1 } from './food1';
 export { Food2Poster, Food2EditorPage, food2 } from './food2';
 export { Food3Poster, Food3EditorPage, food3 } from './food3';
-export * from './food4';
+export { Food4Poster, Food4EditorPage, food4 } from './food4';
 export { Food5Poster, Food5EditorPage, food5 } from './food5';
